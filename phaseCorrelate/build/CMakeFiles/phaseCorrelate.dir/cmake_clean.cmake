7201f155f0f3c43dd5d3ba27636ae25eff6b0d76
chrisliu666 <32890101+chrisliu666@users.noreply.github.com>
2019-06-06 10:24:56 +0800

    Add files via upload

---
+++ phaseCorrelate/build/CMakeFiles/phaseCorrelate.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/phaseCorrelate.dir/phaseCorrelate.cpp.o"
+  "phaseCorrelate.pdb"
+  "phaseCorrelate"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/phaseCorrelate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
